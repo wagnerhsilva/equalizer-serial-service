5196677afb32085914191c0c666fcf85dc2e0525
Flavio Alves <flavio.alves@phiinnovations.com>
2017-06-29 20:00:40 -0300

    correcao de funcao de delay para compilacao

--- service.c
+++ service.c
@@ -293,7 +293,7 @@ int service_start(void) {
 				/*
 				 * Pausa entre as leituras das celulas
 				 */
-				mdelay(params.param1_interbat_delay);
+				usleep(params.param1_interbat_delay);
 			}
 			/*
 			 * Atualiza o valor de target e de tensao de barramento
@@ -353,7 +353,7 @@ int service_start(void) {
 			 * Realiza uma pausa entre as leituras, com valores
 			 * lidos obtidos do banco de dados
 			 */
-			msleep(params.delay);
+			usleep(params.delay);
 			/*
 			 * Reinicia loop de aquisicao de dados de sensor
 			 */
